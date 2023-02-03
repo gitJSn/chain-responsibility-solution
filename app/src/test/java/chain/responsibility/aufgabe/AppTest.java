0de9b163539d02e5673f95e46ec78b8b9142189a
steffen.jaser <steffen.jaser@aracom.de>
2023-02-03 11:03:35 +0100

    add mockito

--- app/src/test/java/chain/responsibility/aufgabe/AppTest.java
+++ app/src/test/java/chain/responsibility/aufgabe/AppTest.java
@@ -1,6 +1,14 @@
 package chain.responsibility.aufgabe;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import chain.responsibility.aufgabe.Interface.IHandler;
+import chain.responsibility.aufgabe.Interface.IUserService;
+import chain.responsibility.aufgabe.handler.IsAdminHandler;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
